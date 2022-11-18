3fda85b6365aa1d6519ba0232be0100c01ad39a4
Guido Visioni <gjvisioni25@vt.edu>
2022-11-18 11:29:57 -0500

    Updated input class, added Webcat test class, added sample input and output files

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -1,5 +1,18 @@
 package prj5;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
 public class Input {
 
+    public static void main(String[] args)
+        throws FileNotFoundException,
+        ParseException {
+        if (args.length >= 1) {
+            InfluencerReader reader = new InfluencerReader(args[0]);
+        }
+        else {
+            InfluencerReader reader = new InfluencerReader("SampleInput1.csv");
+        }
+    }
 }
