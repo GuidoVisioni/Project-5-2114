fdc38d4e4182bb40cc103ac3415b779960aba57c
Guido Visioni <gjvisioni25@vt.edu>
2022-12-02 15:20:06 -0500

    updated input and source files

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -7,33 +7,11 @@ public class Input {
 
     public static void main(String[] args) throws FileNotFoundException {
         if (args.length == 1) {
-            try {
-                if (args[0] == "SampleInput1_2022.csv") {
-                    InfluencerReader reader = new InfluencerReader(
-                        "SampleInput1_2022.csv");
-                }
-                else if (args[0] == "SampleInput2_2022.csv") {
-                    InfluencerReader reader = new InfluencerReader(
-                        "SampleInput2_2022.csv");
-                }
-                else {
-                    InfluencerReader reader = new InfluencerReader(args[0]);
-                }
-            }
-            catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            InfluencerReader reader = new InfluencerReader(args[0]);
         }
-        else {
-            try {
-                InfluencerReader reader = new InfluencerReader(
-                    "SampleInput1_2022.csv");
-            }
-            catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        else
+        {
+            InfluencerReader reader = new InfluencerReader("SampleInput1_2022.csv");
         }
     }
 }
