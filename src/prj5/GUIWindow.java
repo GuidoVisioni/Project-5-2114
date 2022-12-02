9ca514ef18cd730bbf72137343027d0eb514bcc4
hsabbott <hsabbott@vt.edu>
2022-12-02 16:13:58 -0500

    buttons work

--- src/prj5/GUIWindow.java
+++ src/prj5/GUIWindow.java
@@ -39,49 +39,50 @@ public class GUIWindow {
 
         // initialize buttons
         sortByName = new Button("Sort by Channel Name");
-        sortByName.onClick("clickedSortName");
+        sortByName.onClick(this, "clickedSortName");
         window.addButton(sortByName, WindowSide.NORTH);
 
         sortByEngagementRate = new Button("Sort by Engagement Rate");
-        sortByEngagementRate.onClick("clickedSortEngagement");
+        sortByEngagementRate.onClick(this, "clickedSortEngagement");
         window.addButton(sortByEngagementRate, WindowSide.NORTH);
 
         quit = new Button("Quit");
-        quit.onClick("clickedQuit");
+        quit.onClick(this, "clickedQuit");
         window.addButton(quit, WindowSide.NORTH);
 
         traditionalEngage = new Button("Traditional Engagement Rate");
-        traditionalEngage.onClick("clickedTradEngage");
+        traditionalEngage.onClick(this, "clickedTradEngage");
         window.addButton(traditionalEngage, WindowSide.WEST);
 
         reachEngage = new Button("Reach Engagement");
-        reachEngage.onClick("clickedReachEngage");
+        reachEngage.onClick(this, "clickedReachEngage");
         window.addButton(reachEngage, WindowSide.WEST);
 
         janButton = new Button("January");
-        janButton.onClick("clickedMonth");
+        janButton.onClick(this, "clickedMonth");
         window.addButton(janButton, WindowSide.SOUTH);
 
         febButton = new Button("February");
-        febButton.onClick("clickedMonth");
+        febButton.onClick(this, "clickedMonth");
         window.addButton(febButton, WindowSide.SOUTH);
 
         marchButton = new Button("March");
-        marchButton.onClick("clickedMonth");
+        marchButton.onClick(this, "clickedMonth");
         window.addButton(marchButton, WindowSide.SOUTH);
 
         quarterButton = new Button("First Quarter (Jan - March)");
-        quarterButton.onClick("clickedQuarter");
+        quarterButton.onClick(this, "clickedQuarter");
         window.addButton(quarterButton, WindowSide.SOUTH);
         
         timeFrame = new TextShape(1, 1, "January");
         window.addShape(timeFrame);
+
+        engagementType = new TextShape(1, 21, " Traditional Engagement Rate");
+        window.addShape(engagementType);
         
-        sortingLabel = new TextShape(1, 21, "Traditional Engagement Rate");
+        sortingLabel = new TextShape(1, 41, "Sorting by Engagement Rate");
         window.addShape(sortingLabel);
         
-        engagementType = new TextShape(1, 41, "Sorting by Engagement Rate");
-        window.addShape(engagementType);
         
         window.addShape(new Shape(100, 100, 10, 10));
     }
@@ -91,12 +92,12 @@ public class GUIWindow {
 
 
     public void clickedSortName(Button button) {
-        
+        sortingLabel.setText("Sorting by Channel Name");
     }
 
 
     public void clickedSortEngagement(Button button) {
-
+        sortingLabel.setText("Sorting by Engagement Rate");
     }
 
 
@@ -106,22 +107,33 @@ public class GUIWindow {
 
 
     public void clickedTradEngage(Button button) {
-
+        engagementType.setText("Traditional Engagement Rate");
     }
 
 
     public void clickedReachEngage(Button button) {
-
+        engagementType.setText("Reach Engagement Rate");
     }
 
 
     public void clickedMonth(Button button) {
-
+        String month = button.getTitle().toLowerCase();
+        switch (month) {
+            case "january":
+                timeFrame.setText("January");
+            case "february":
+                timeFrame.setText("February");
+            case "march":
+                timeFrame.setText("March");
+            default:
+                timeFrame.setText("January");
+        }
+        
     }
 
 
     public void clickedQuarter(Button button) {
-
+        timeFrame.setText("First Quarter (Jan - March)");
     }
 
 
