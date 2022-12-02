00b7d763953053407a3a597563b00b243d0657d9
Guido Visioni <gjvisioni25@vt.edu>
2022-12-02 17:28:52 -0500

    removed output files

--- src/prj5/ComparatorAlphabetical.java
+++ src/prj5/ComparatorAlphabetical.java
@@ -6,12 +6,20 @@ package prj5;
 import java.util.Comparator;
 
 /**
+ * Comparator for alphabetical
+ * 
  * @author rama04, hsabbott, gjvisioni25
  * @version 11/18/2022
  *
  */
 public class ComparatorAlphabetical implements Comparator<Influencer> {
 
+    /**
+     * Compares the two influencers alphabetically
+     * 
+     * @return
+     *         -1, 0, or 1
+     */
     @Override
     public int compare(Influencer one, Influencer two) {
         return one.getChannelName().toLowerCase().compareTo(two.getChannelName()
--- src/prj5/ComparatorER.java
+++ src/prj5/ComparatorER.java
@@ -6,14 +6,20 @@ package prj5;
 import java.util.Comparator;
 
 /**
- * Comparator EngagementReach
+ * Comparator for engagement reach
  * 
- * @author ramamittal
+ * @author rama04, hsabbott, gjvisioni25
  * @version 11/15/2022
  *
  */
 public class ComparatorER implements Comparator<Influencer> {
 
+    /**
+     * Compares the two influencers by reach engagement rate
+     * 
+     * @return
+     *         -1, 0, or 1
+     */
     @Override
     public int compare(Influencer one, Influencer two) {
         if (one.getPosts() < two.getPosts()) {
